b96702cbd4a21281875c1e5bb601d2c0d34fd572
yash <injose.joshi@gmail.com>
2024-07-24 15:25:10 +0530

    Add cosmetic refactoring

--- src/db/database.ts
+++ src/db/database.ts
@@ -4,10 +4,10 @@ import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 declare global {
-  var database: PostgresJsDatabse<typeof schema> | undefined;
+  var database: PostgresJsDatabase<typeof schema> | undefined;
 };
 
-let database: PostgresJsDatabse<typeof schema>;
+let database: PostgresJsDatabase<typeof schema>;
 let pg: ReturnType<typeof postgres>;
 
 if (env.Node_ENV === "production") {
